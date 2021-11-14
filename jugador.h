02a82d761be4bcea315bdfc1de8b1a0d2175da3e
BMQuiroga <bquiroga@fi.uba.ar>
2021-11-13 22:52:50 -0300

    cosas

---
+++ jugador.h
@@ -0,0 +1,30 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include "menu.h"
+
+
+class Jugador{
+
+private:
+    std::string nombre;
+    ListaMateriales materiales;
+    int energia;
+
+public:
+    void mover();
+    void sumar_energia(int cantidad)
+    void restar_energia(int cantidad)
+
+
+
+
+
+
+};
+
+
+
+
+
+
+#endif
