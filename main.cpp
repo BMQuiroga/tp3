daaea4d7d9539d88548810951d79b9e319673dc6
Franco <fmacke10@gmail.com>
2021-12-10 18:53:40 -0300

    Main ahora lee todos los archivos
    REVISAR lee mal posicion jugadores

--- main.cpp
+++ main.cpp
@@ -9,22 +9,29 @@ using namespace std;
 int main(){
     Archivo archivo;
     Utilidad util;
-    ListaEdificios edificios;
+
+    ListaEdificios* edificios = new ListaEdificios;
+    edificios->procesar_archivo_edificios();      
+
     ListaMateriales materiales1, materiales2;
+    archivo.procesar_archivo_materiales(materiales1,materiales2);   //tuve que hacerlo asi porque sino perdia scope las variables y me devolvia 0 el primer puntero
+    
     Mapa* mapa = archivo.procesar_archivo_mapa();
-    Jugador* jugador1 = new Jugador(1,1,1, &edificios, mapa);
-    Jugador* jugador2 = new Jugador(2,2,2, &edificios, mapa);
+
+    Jugador* jugador1 = new Jugador(1,1,1, edificios, mapa);
+    Jugador* jugador2 = new Jugador(2,2,2, edificios, mapa);
+
+    mapa->procesar_archivo_ubicaciones(edificios, jugador1, jugador2);
+
+    jugador1->asignar_lista_materiales(&materiales1);
+    jugador2->asignar_lista_materiales(&materiales2);
     // Jugador jugador1(1,1,1, edificios, mapa);
     // Jugador jugador2(2,2,2, edificios, mapa);
     
-    edificios.procesar_archivo_edificios();               
-    archivo.procesar_archivo_materiales(materiales1,materiales2);   //tuve que hacerlo asi porque sino perdia scope las variables y me devolvia 0 el primer puntero
-    jugador1->asignar_lista_materiales(&materiales1);
-    jugador2->asignar_lista_materiales(&materiales2);
 
     // jugador2.devolver_coordenada_x(materiales2);
 
-    Menu menu(mapa, &edificios, jugador1, jugador2);
+    Menu menu(mapa, edificios, jugador1, jugador2);
 
     //ListaObjetivos vector_objetivos_completos1(jugador1, materiales1, edificios, mapa);
     //jugador1.asignar_objetivos_aleatorios(ListaObjetivos::obtener_objetivos_aleatorios())
