63492613d67a4dbf033917c23df16d1bc96c3ff3
Arely Agama <aagama@fi.uba.ar>
2021-12-13 11:23:28 -0300

    pre y pos

--- Energetico.h
+++ Energetico.h
@@ -12,9 +12,20 @@ private:
     bool cumplio;
 public:
 
+    //Pre:-
+    //Pos:Muestra por pantalla el objetivo, y si cumple muestra que fue completado
     void mostrar();
+
+    //Pre: Recibe el jugador de la partida
+    //Pos: Inicializa los atributos, energia_maxima = 100
     Energetico(Jugador * jugador);
+
+    //Pre:
+    //Pos:
     int calcular_progreso();
+
+    //Pre:-
+    //Pos: Devuelve true si la energia es mayor o igual que 100 (energia maxima)
     bool checkear();
 
 };
